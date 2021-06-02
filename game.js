baeee31474b9ff70775354393768210b71fb4c06
complexitycollapse <complexitycollapse@protonmail.com>
2021-06-02 12:31:49 +0100

    Refactoring and mouse handling

--- game.js
+++ game.js
@@ -1,19 +1,56 @@
 (function () {
-    var canvas = document.getElementById("game");
-    var requestAnimationFrame = (function () {
-        var w = window;
-        return w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
-    })();
+    var then;
 
-    function gameLoop() {
-        var now = Date.now();
-        var delta = (now - then) / 1000;
-        oldMain(delta);
+    // The main loop of the game. Call to start.
+    var gameLoop = function (canvas, mouseEvents) {
+        var ctx = canvas.getContext("2d");
 
-        then = now;
-        requestAnimationFrame(gameLoop);
+        var requestAnimationFrame = (function () {
+            var w = window;
+            return w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+        }());
+
+        function innerLoop() {
+            var now = Date.now();
+            var delta = (now - then) / 1000;
+            oldMain(delta, ctx);
+
+            then = now;
+            requestAnimationFrame(innerLoop);
+        }
+        var oldMain = oldGame(canvas);
+        then = Date.now();
+        innerLoop();
+    };
+
+    // Attach mouse event handlers
+    var initMouseEvents = function (canvas) {
+        var mouseEvents = {
+            mouseX: 0,
+            mouseY: 0,
+            mouseOver: false
+        };
+
+        canvas.addEventListener("mousemove", function (e) {
+            var bounds = e.target.getBoundingClientRect();
+            mouseEvents.mouseX = e.clientX - bounds.left;
+            mouseEvents.mouseY = e.clientY - bounds.top;
+        });
+
+        canvas.addEventListener("mouseover", function () {
+            mouseEvents.mouseOver = true;
+        });
+
+        canvas.addEventListener("mouseout", function () {
+            mouseEvents.mouseOver = false;
+        });
+
+        return mouseEvents;
+    };
+
+    {
+        var canvas = document.getElementById("game");
+        let mouseEvents = initMouseEvents(canvas);
+        gameLoop(canvas, mouseEvents);
     }
-    var oldMain = oldGame(canvas);
-    var then = Date.now();
-    gameLoop();
-})()
+}());
